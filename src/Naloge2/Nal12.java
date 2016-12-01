a51cb6147e503aeec405a499c7bac3a0d86d0e4f
Timotej <timotej.manfreda@gmail.com>
2016-12-01 10:15:59 +0100

    Dodana naloga 12

---
+++ src/Naloge2/Nal12.java
@@ -0,0 +1,29 @@
+package Naloge2;
+
+import java.util.Scanner;
+
+public class Nal12 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(MMtoM(x));
+        System.out.println(MMtoDM(x));
+        System.out.println(MMtoCM(x));
+    }
+
+    public static int MMtoM(int x) {
+        return x/1000;
+    }
+
+    public static int MMtoDM(int x) {
+        return x/100;
+    }
+
+    public static int MMtoCM(int x) {
+        return x/10;
+    }
+
+}
