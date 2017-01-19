444af636159d76353319ad8bf5348ed40fdd688f
Timotej <timotej.manfreda@gmail.com>
2017-01-19 09:28:47 +0100

    Sum enchanced

--- src/Naloge3/Nal9.java
+++ src/Naloge3/Nal9.java
@@ -3,11 +3,28 @@ package Naloge3;
 public class Nal9 {
     public static void main(String[] args) {
         System.out.println(sum(14,2));
+        System.out.println(sum(-14,2));
+        System.out.println(sum(14,-2));
+        System.out.println(sum(-14,-2));
+        /*
+            16
+            -12
+            12
+            -16
+         */
     }
 
-    public static int sum(int n1, int n2) {
+    public static int sum(int x, int y) {
+        if(x == 0)
+            return y;
+        if(x < 0)
+            return -sum(-x-1, -y)-1;
+        return sum(x-1, y+1);
+    }
+
+    /*public static int sum(int n1, int n2) {
         if(n2 == 0)
             return n1;
         return 1+sum(n1,n2-1);
-    }
+    }*/
 }
